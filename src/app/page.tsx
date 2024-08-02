9604fdce99ad44f5990e45c18b92c194773ccc36
Jaime Dias <jaimejosediasnt@gmail.com>
2024-08-02 18:06:54 -0300

    logo basepath

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 
+//https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
         <Image
           className="dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/logo_reaprev.png"
+          src="reaprev/logo_reaprev.png"
           alt="Reaprev Logo"
           width={189}
           height={136}
